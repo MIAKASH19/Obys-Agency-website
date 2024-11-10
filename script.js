b99c846e20fa03814ed3abd182489dd12c4a642a
MI AKASH <mszharul1990@gmail.com>
2024-11-10 22:10:52 +0600

    starting hero section

--- script.js
+++ script.js
@@ -1,55 +1,62 @@
-let tl = gsap.timeline();
-
 // Loader Animation Start
 
+function loaderAnimation() {
+  let tl = gsap.timeline();
 
-tl.from(".line h1, .line h2", {
-  y: 150,
-  stagger: 0.3,
-  duration: 0.6,
-  delay: 0.5,
-});
+  tl.from(".line h1, .line h2", {
+    y: 150,
+    stagger: 0.3,
+    duration: 0.6,
+    delay: 0.5,
+  });
 
-tl.from(".loader-text", {
-  opacity: 0,
-  duration: 0.4,
-}, "anim")
+  tl.from(
+    ".loader-text",
+    {
+      opacity: 0,
+      duration: 0.4,
+    },
+    "anim"
+  );
 
-tl.from(".line1-part1", {
-  opacity: 0,                  
-  onStart: function () {
-    let line1h5 = document.querySelector(".line1-part1 h5");
-    let counter = 0;
+  tl.from(
+    ".line1-part1",
+    {
+      opacity: 0,
+      onStart: function () {
+        let line1h5 = document.querySelector(".line1-part1 h5");
+        let counter = 0;
 
-    const intervalId = setInterval(() => {
-      counter++;
-      line1h5.textContent = counter;
+        const intervalId = setInterval(() => {
+          counter++;
+          line1h5.textContent = counter;
 
-      if (counter >= 100) {
-        clearInterval(intervalId);
-      }
-    }, 25);
-  },
-}, "anim");
+          if (counter >= 100) {
+            clearInterval(intervalId);
+          }
+        }, 25);
+      },
+    },
+    "anim"
+  );
 
-tl.to(".line , .loader-text", {
-  opacity: 0,
-  duration: 1,
-  delay: 2.3,
-  onComplete: function () {
-    document.querySelector("main").style.display = "block";
-  },
-});
+  tl.to(".line , .loader-text", {
+    opacity: 0,
+    duration: 1,
+    delay: 2.3,
+    onComplete: function () {
+      document.querySelector("main").style.display = "block";
+    },
+  });
 
-tl.to("#loader", {
-  y: "-100%",
-  ease: "power3.out",
-  duration: 0.5,
-  onComplete: function () {
-    document.querySelector("#loader").style.display = "none";
-  },
-});
-
+  tl.to("#loader", {
+    y: "-100%",
+    ease: "power3.out",
+    duration: 0.5,
+    onComplete: function () {
+      document.querySelector("#loader").style.display = "none";
+    },
+  });
+}
+loaderAnimation()
 // Loader Animation End
-
-
